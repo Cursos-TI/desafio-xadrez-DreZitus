0bd0761cbf399c9497a6da5cad7230c3bffd034b
DreZitus <108902451+DreZitus@users.noreply.github.com>
2025-07-30 22:41:16 +0000

    Atualização do programa completo com correções menos complexas usando loops

--- xadrez.c
+++ xadrez.c
@@ -6,16 +6,26 @@
 
 int main() {
     
-    int casas;
-    int i;
-    char movimento[50];
 
-    for (i = 0; i < 1; i++){
-        printf("Digite quantas casas a Torre anda\n");
-        scanf("%d", &casas);
-        printf("digite a direção do movimento da Torre\n");
-        scanf("%d", movimento);
-        printf("torre andou: %d casas, na direção: %s\n", casas, movimento);
+    for(int i = 0; i < 5; i++){
+        printf("Torre: Direita \n");
+    }
+
+    for(int i = 0; i < 5; i++){
+        for(int i = 0; i < 2; i++){
+            printf("Bispo: esquerda");
+        }
+        print("Bispo: cima");
+    }
+
+
+    int movimentocompleto = 1;
+
+    while (movimentocompleto--){
+        for(int i = 0; i < 2; i++){
+            printf("Cavalo: cima\n");
+        }
+        printf("Cavalo: Direita\n");
     }
 
     return 0;
