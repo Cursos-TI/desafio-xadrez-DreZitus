1eef77a6d9cdd3e6672ca7224493ed851634f8d7
DreZitus <108902451+DreZitus@users.noreply.github.com>
2025-08-01 00:36:38 +0000

    implementação de logicas mais complexas

--- xadrez.c
+++ xadrez.c
@@ -4,38 +4,60 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+//Movimento Torre
+void movimentoTorre(){
     
-    //Movimento Torre
     for(int i = 0; i < 5; i++){
-        printf("Torre: Direita \n");
+        printf("Torre: Direita\n");
     }
 
-    //Movimento Bispo
-    for(int i = 0; i < 5; i++){
-        printf("Bispo: Cima, Direita\n");
+}
 
+//Movimento Bispo
+void movimentoBispo(){
+    int movimentoCompleto = 1;
+    while (movimentoCompleto--){
+        for (int i = 0, j = 5; i < j || j > i; i++, j--)
+        {
+            printf("Bispo: Direita\n");
+            printf("Bispo: cima\n");
+        }
+        
     }
+    
 
-    //Movimento Rainha
+}
+
+//Movimento Rainha
+void movimentoRainha(){
 
     int passosRainha = 8;
-
-    while(passosRainha > 0){
-        printf("Rainha: Esquerda \n");
-        passosRainha--;
+    while(passosRainha > 0){ 
+    printf("Rainha: Esquerda\n");
+    passosRainha--;
     }
-
     
+}
+
+//Movimento Cavalo
+void movimentoCavalo(){
     int movimentocompleto = 1;
 
-    //Movimento do Cavalo com loop aninhado
     while (movimentocompleto--){
-        for(int i = 0; i < 2; i++){
+        for(int i = 0, j = 3; i < j || j > i; i++, j--){
             printf("Cavalo: cima\n");
         }
         printf("Cavalo: Direita\n");
     }
+
+}
+
+int main() {
+
+    movimentoTorre();
+    movimentoBispo();
+    movimentoRainha();
+    movimentoCavalo();
 
     return 0;
 }
